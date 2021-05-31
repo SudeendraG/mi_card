7f725570974d5fb5681ea0a01bc083e7fe41b76e
SudeendraG <sudeendracse@gmail.com>
2021-05-31 17:07:24 +0530

    inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,112 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: AssetImage('images/profile.jpeg'),
+                ),
+                Text(
+                  'Sudeendra',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'),
+                ),
+                Text(
+                  'APPLICATION DEVELOPER',
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.teal.shade100,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'SourceSansPro'),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 250,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+91-9898989890',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro'),
+                      ),
+                    )),
+                /*Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10.0),
+                  margin:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '+91-999999273',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),*/
+                Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'sudeendracse@gmail.com',
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro'),
+                      ),
+                    ))
+              ]),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
